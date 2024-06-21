4cb6283a79d360a2f4b631db0d8430ac66e7acfb
Martin Culak <culak.martin@gmail.com>
2024-06-21 11:19:18 +0200

    vymazane items

--- src/router.js
+++ src/router.js
@@ -1,10 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
-
 const UserAuth = () => import('./pages/auth/UserAuth.vue');
-
-import ItemsList from './pages/items/ItemsList.vue';
-import ItemAdd from './pages/items/ItemAdd.vue';
-import itemEdit from './pages/items/ItemEdit.vue';
 
 import TripList from './pages/trips/TripList.vue';
 import TripAdd from './pages/trips/TripAdd.vue';
@@ -21,9 +16,6 @@ const router = createRouter({
 	history: createWebHistory(),
 	routes: [
 		{ path: '/', component: indexPage },
-		{ name: 'items', path: '/items', component: ItemsList, meta: { requiresAuth: true } },
-		{ name: 'item-add', path: '/items/add', component: ItemAdd, meta: { requiresAuth: true } },
-		{ name: 'item-edit', path: '/items/:itemId', component: itemEdit, props: true, meta: { requiresAuth: true } },
 
 		{ name: 'trips', path: '/trips', component: TripList, meta: { requiresAuth: true } },
 		{ name: 'trip-add', path: '/trip/add', component: TripAdd, meta: { requiresAuth: true } },
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,9 @@
 import { createStore } from 'vuex';
-import itemsModule from './modules/items/index.js';
 import tripsModule from './modules/trips/index.js';
 import authModule from './modules/auth/index.js';
 
 const store = createStore({
 	modules: {
-		items: itemsModule,
 		trips: tripsModule,
 		auth: authModule
 	}
