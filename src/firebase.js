2e24b30577436c410054d9c6645601a535235b71
Martin Culak <culak.martin@gmail.com>
2024-12-19 23:53:21 +0100

    pridane emulatory auth, storage

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { connectAuthEmulator, getAuth } from 'firebase/auth';
 import { initializeFirestore, getFirestore, connectFirestoreEmulator, memoryLocalCache } from 'firebase/firestore';
+import { getStorage, connectStorageEmulator } from 'firebase/storage';
 
 const firebaseConfig = {
 	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -19,7 +20,10 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 // export const db = initializeFirestore(app, {localCache: memoryLocalCache()});
+// export const storage = getStorage(app);
 
 if (process.env.NODE_ENV === 'development') {
 	connectFirestoreEmulator(db, 'localhost', 8080);
+	connectAuthEmulator(auth, 'http://localhost:9099');
+	//connectStorageEmulator(storage, 'localhost', 9199);
 }
