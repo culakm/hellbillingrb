9c77fb70042c44e42348641dc18586f81aaf86eb
Martin Culak <culak.martin@gmail.com>
2025-05-28 19:49:04 +0200

    pred prestavbou na COMPOSITION API

--- src/plugins/utilsPlugin.js
+++ src/plugins/utilsPlugin.js
@@ -3,6 +3,8 @@ import { loadErrorMessage, handleError, printNumber } from '../libs/utils.js';
 export default {
 	install(app) {
 		app.config.globalProperties.$loadErrorMessage = loadErrorMessage;
+		// Registering the utility functions globally
+		// V komponente sa to pouziva this.$printNumber(41);
 		app.config.globalProperties.$printNumber = printNumber;
 		app.config.globalProperties.$handleError = handleError;
 	}
