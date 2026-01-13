8df7aaaaf925a15bc47aadf60dbacec05f658991
Martin Culak <culak.martin@gmail.com>
2026-01-13 22:13:08 +0100

    vyhadzane components/ui/Base...

--- src/main.js
+++ src/main.js
@@ -1,14 +1,9 @@
-import { createApp, defineAsyncComponent } from "vue";
+import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "@/router";
 import utilsPlugin from "./plugins/utilsPlugin.js";
-import BaseCard from "./components/ui/BaseCard.vue";
-import BaseButton from "./components/ui/BaseButton.vue";
-import BaseBadge from "./components/ui/BaseBadge.vue";
-import BaseSpinner from "./components/ui/BaseSpinner.vue";
 import Container from "./components/ui/Container.vue";
-const BaseDialog = defineAsyncComponent(() => import("./components/ui/BaseDialog.vue"));
 
 import { Quasar, Loading, Dialog, AppFullscreen } from "quasar";
 import "@quasar/extras/material-icons/material-icons.css";
@@ -22,11 +17,6 @@ app.use(router);
 app.use(utilsPlugin);
 app.use(Quasar, { plugins: { Loading, Dialog, AppFullscreen } });
 
-app.component("base-card", BaseCard);
-app.component("base-button", BaseButton);
-app.component("base-badge", BaseBadge);
-app.component("base-spinner", BaseSpinner);
-app.component("base-dialog", BaseDialog);
 app.component("Container", Container);
 
 app.mount("#app");
